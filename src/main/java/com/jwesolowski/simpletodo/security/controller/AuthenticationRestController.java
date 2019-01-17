94af5cbc5ff83a56d1adc6e6a99993c812ca9a0b
jakub.wesolowski@spagee.com <jakub.wesolowski@spagee.com>
2019-01-17 21:26:04 +0100

    Fix cors on security rest controller

--- src/main/java/com/jwesolowski/simpletodo/security/controller/AuthenticationRestController.java
+++ src/main/java/com/jwesolowski/simpletodo/security/controller/AuthenticationRestController.java
@@ -17,6 +17,7 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class AuthenticationRestController {
 
   @Value("${jwt.header}")
