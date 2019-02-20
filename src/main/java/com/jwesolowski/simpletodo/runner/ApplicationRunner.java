e390a5fa879a0ba10dcea15452cc6668f0d69e65
jakub.wesolowski@spagee.com <jakub.wesolowski@spagee.com>
2019-02-20 18:29:25 +0100

    Fix cors

--- src/main/java/com/jwesolowski/simpletodo/runner/ApplicationRunner.java
+++ src/main/java/com/jwesolowski/simpletodo/runner/ApplicationRunner.java
@@ -4,9 +4,13 @@ import it.ozimov.springboot.mail.configuration.EnableEmailTools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EntityScan("com.jwesolowski.simpletodo.domain")
@@ -19,5 +23,15 @@ public class ApplicationRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(ApplicationRunner.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:9000");
+      }
+    };
   }
 }
