6c57ef9e122410ce7d8af98a3246ac55b2c05f77
jakub.wesolowski@spagee.com <jakub.wesolowski@spagee.com>
2019-02-20 18:42:43 +0100

    Fix cors

--- src/main/java/com/jwesolowski/simpletodo/runner/ApplicationRunner.java
+++ src/main/java/com/jwesolowski/simpletodo/runner/ApplicationRunner.java
@@ -30,7 +30,7 @@ public class ApplicationRunner {
     return new WebMvcConfigurerAdapter() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:9000");
+        registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:4200");
       }
     };
   }
